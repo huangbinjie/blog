d63244bef3770e23d093f5adf9adc78faa1b8917
bingjie.huang <bingjie.huang@shuyun.com>
2017-08-25 18:09:21 +0800

    static 配置

--- lib/blog_web/endpoint.ex
+++ lib/blog_web/endpoint.ex
@@ -8,8 +8,8 @@ defmodule BlogWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :blog, gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :blog, gzip: true
+    # only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
